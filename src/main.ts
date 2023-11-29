e59a0fd5e1cc33fa9d9733418e0ea569b1042503
Crawron <azertyaaron@gmail.com>
2023-11-29 00:11:00 -0500

    Add two pin reactions to pin

--- src/main.ts
+++ src/main.ts
@@ -16,11 +16,29 @@ import { garnetCountCommand, tradeCommand } from "./commands/definitions/trade"
 import { rollCommand } from "./commands/definitions/roll"
 
 const discordClient = new Client({
-	intents: ["GuildMessages", "Guilds", "MessageContent", "GuildMembers"],
+	intents: [
+		"GuildMessages",
+		"Guilds",
+		"MessageContent",
+		"GuildMembers",
+		"GuildMessageReactions",
+	],
 })
 
 discordClient.on("ready", () => console.info("🦮 Ready"))
 discordClient.on("messageCreate", handleMirror)
+discordClient.on("messageReactionAdd", async (reaction, user) => {
+	if (reaction.emoji.name !== "📌") return
+
+	const message = await reaction.message.fetch(true)
+
+	const hasEnoughReactions = message.reactions.cache.some(
+		(r) => r.emoji.name === "📌" && r.count >= 2
+	)
+
+	if (hasEnoughReactions) message.pin("Pin reactions")
+	else message.unpin()
+})
 
 handleCommands(discordClient, env.DISCORD_SERVER_ID, [
 	petCommand,
